ec69d41ed2e582deaed0aee606293891dc1bd899
PheasantZelda <104151600+PheasantZelda@users.noreply.github.com>
2022-11-23 20:07:49 +0900

    Add files via upload

---
+++ js/canvas.js
@@ -0,0 +1,51 @@
+var $target1 = document.querySelector('.MU_box1')
+var $button1 = document.querySelector('.button1')
+$button1.addEventListener('click', function () {
+  $target1.classList.toggle('is-hidden')
+})
+var $target2 = document.querySelector('.MU_box2')
+var $button2 = document.querySelector('.button2')
+$button2.addEventListener('click', function () {
+  $target2.classList.toggle('is-hidden')
+})
+var $target3 = document.querySelector('.MU_box3')
+var $button3 = document.querySelector('.button3')
+$button3.addEventListener('click', function () {
+  $target3.classList.toggle('is-hidden')
+})
+var $target4 = document.querySelector('.MU_box4')
+var $button4 = document.querySelector('.button4')
+$button4.addEventListener('click', function () {
+  $target4.classList.toggle('is-hidden')
+})
+var $target5 = document.querySelector('.MU_box5')
+var $button5 = document.querySelector('.button5')
+$button5.addEventListener('click', function () {
+  $target5.classList.toggle('is-hidden')
+})
+var $target6 = document.querySelector('.MU_box6')
+var $button6 = document.querySelector('.button6')
+$button6.addEventListener('click', function () {
+  $target6.classList.toggle('is-hidden')
+})
+var $target7 = document.querySelector('.MU_box7')
+var $button7 = document.querySelector('.button7')
+$button7.addEventListener('click', function () {
+  $target7.classList.toggle('is-hidden')
+})
+var $target8 = document.querySelector('.MU_box8')
+var $button8 = document.querySelector('.button8')
+$button8.addEventListener('click', function () {
+  $target8.classList.toggle('is-hidden')
+})
+var $target9 = document.querySelector('.MU_box9')
+var $button9 = document.querySelector('.button9')
+$button9.addEventListener('click', function () {
+  $target9.classList.toggle('is-hidden')
+})
+var $target10 = document.querySelector('.MU_box10')
+var $button10 = document.querySelector('.button10')
+$button10.addEventListener('click', function () {
+  $target10.classList.toggle('is-hidden')
+})
+
