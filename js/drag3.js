1cecfc31dc0fbab84912b2b10a1a96a6f584573b
PheasantZelda <104151600+PheasantZelda@users.noreply.github.com>
2023-02-10 20:17:25 +0900

    Add files via upload

---
+++ js/drag3.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+  const drag={target:null,mouseon:0};
+  const box2=document.querySelectorAll('.box');
+  document.addEventListener('mousedown',e=>{
+    const t=e.target;
+    if(t.matches('.item')){
+      drag.target=t;
+      drag.mouseon=1;
+      drag.target.classList.add('dragging');
+    }
+  });
+  document.addEventListener("dragover",e=> {
+    e.preventDefault();
+    const t=e.target;
+    box2.classList.toggle('over',t.closest('.box') && drag.mouseon);
+  });
+  document.addEventListener("mouseup",e=> {
+    drag.mouseon=0;
+    drag.target.classList.remove('dragging');
+  });
+  document.addEventListener('drop',e=>{
+    const t=e.target;
+    if(t.matches('.box')){
+      t.appendChild(drag.target);
+    }
+    if(t.matches('.box .item')){
+      t.after(drag.target);
+    }
+    drag.target.classList.remove('dragging');
+    drag.target=null;
+    drag.mouseon=0;
+    box2.classList.remove('over');
+  });
+});
