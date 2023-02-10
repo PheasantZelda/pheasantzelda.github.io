5d4c8974177b04f9dad15927cf859526ba0dd911
PheasantZelda <104151600+PheasantZelda@users.noreply.github.com>
2023-02-10 22:28:50 +0900

    Add files via upload

--- js/drag.js
+++ js/drag.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', ()=>{
   const drag={target:null,mouseon:0};
   const box=document.querySelectorAll('.box');
-  document.addEventListener('mousedown',e=>{
+  document.addEventListener('touchstart',e=>{
     const t=e.target;
     if(t.matches('.item')){
       drag.target=t;
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
     const t=e.target;
     box.classList.toggle('over',t.closest('.box') && drag.mouseon);
   });
-  document.addEventListener("mouseup",e=> {
+  document.addEventListener("touchend",e=> {
     drag.mouseon=0;
     drag.target.classList.remove('dragging');
   });
@@ -29,30 +29,6 @@ window.addEventListener('DOMContentLoaded', ()=>{
     drag.target.classList.remove('dragging');
     drag.target=null;
     drag.mouseon=0;
-    box.classList.remove('over');
-  });
-  document.addEventListener("touchstart", e=>{
-    const t=e.target;
-    if(t.matches('.item')){
-      drag.target=t;
-      drag.target.classList.add('dragging');
-    }
-  });
-  document.addEventListener("touchmove", e=> {
-    e.preventDefault();
-    const t=e.target;
-    box.classList.toggle('over',t.closest('.box'));
-  });
-  document.addEventListener("touchend", e=> {
-    const t=e.target;
-    if(t.matches('.box')){
-      t.appendChild(drag.target);
-    }
-    if(t.matches('.box .item')){
-      t.after(drag.target);
-    }
-    drag.target.classList.remove('dragging');
-    drag.target=null;
     box.classList.remove('over');
   });
 });
