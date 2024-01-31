1738d9781301cddadf7ee0f3c5cf5f96c91abc1e
pheasant <pheasant.smash@gmail.com>
2024-01-31 14:15:31 +0900

    upd

--- js/drag_phone.js
+++ js/drag_phone.js
@@ -1,24 +1,16 @@
 let draggedItem = null;
-let startX = 0;
-let startY = 0;
 
-const nonImageArea = document.getElementById('nonImageArea');
-const mappingAreas = document.querySelectorAll('.MU_result');
-const draggableItems = document.querySelectorAll('.item');
-
-function handleDragStart(e) {
-  draggedItem = e.target;
-}
-
-function handleDragEnd() {
-  draggedItem = null;
-}
+const nonImageArea = document.getElementById("nonImageArea");
+const mappingAreas = document.querySelectorAll(".MU_result");
+const draggableItems = document.querySelectorAll(".item");
 
 function handleTouchStart(e) {
   draggedItem = e.target;
   let touch = e.touches[0];
   startX = touch.clientX - parseFloat(draggedItem.style.left || 0);
+  console.log("startX: %s ", startX);
   startY = touch.clientY - parseFloat(draggedItem.style.top || 0);
+  console.log("startY: %s ", startY);
   e.preventDefault();
 }
 
@@ -26,17 +18,28 @@ function handleTouchMove(e) {
   if (!draggedItem) return;
   let touch = e.touches[0];
   draggedItem.style.left = `${touch.clientX - startX}px`;
+  console.log("moveX: %s ", draggedItem.style.left);
   draggedItem.style.top = `${touch.clientY - startY}px`;
+  console.log("moveY: %s ", draggedItem.style.top);
   e.preventDefault();
+  e.target.classList.add("active");
 }
 
 function handleTouchEnd(e) {
+  e.target.classList.remove("active");
   if (!draggedItem) return;
 
   mappingAreas.forEach((mappingArea) => {
     const rect = mappingArea.getBoundingClientRect();
+    console.log("rect: %s ", rect);
     let left = parseFloat(draggedItem.style.left) + startX;
     let top = parseFloat(draggedItem.style.top) + startY;
+    console.log("left: %s ", left);
+    console.log("top: %s ", top);
+    console.log("rect.left: %s ", rect.left);
+    console.log("rect.top: %s ", rect.top);
+    console.log("rect.bottom: %s ", rect.bottom);
+    console.log("rect.right: %s ", rect.right);
 
     if (
       left >= rect.left &&
@@ -49,26 +52,14 @@ function handleTouchEnd(e) {
       const y = touch.clientY - rect.top;
       draggedItem.style.left = `${x - draggedItem.offsetWidth / 2}px`;
       draggedItem.style.top = `${y - draggedItem.offsetHeight / 2}px`;
-      draggedItem.crossOrigin = 'anonymous';
-      mappingArea.appendChild(draggedItem);
+      draggedItem.crossOrigin = "anonymous";
+      mappingArea.append(draggedItem);
     }
   });
-
-  if (!draggedItem.parentNode) {
-    // マッピングエリア外に出た場合
-    draggedItem.style.left = ''; // スタイルをリセット
-    draggedItem.style.top = ''; // スタイルをリセット
-    nonImageArea.appendChild(draggedItem);
-  }
-
-  draggedItem = null;
-  e.preventDefault();
 }
 
 draggableItems.forEach((item) => {
-  item.addEventListener('dragstart', handleDragStart);
-  item.addEventListener('dragend', handleDragEnd);
-  item.addEventListener('touchstart', handleTouchStart);
-  item.addEventListener('touchmove', handleTouchMove);
-  item.addEventListener('touchend', handleTouchEnd);
+  item.addEventListener("touchstart", handleTouchStart);
+  item.addEventListener("touchmove", handleTouchMove);
+  item.addEventListener("touchend", handleTouchEnd);
 });
