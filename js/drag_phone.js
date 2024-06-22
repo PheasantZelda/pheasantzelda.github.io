2ef46dbddc3b7070c0b9545dd8b68f5edeae3351
pheasant <pheasant.smash@gmail.com>
2024-06-22 21:34:36 +0900

    upd

--- js/drag_phone.js
+++ js/drag_phone.js
@@ -3,6 +3,26 @@ let draggedItem = null;
 const nonImageAreas = document.querySelectorAll('.Fighter_box');
 const mappingAreas = document.querySelectorAll('.MU_result');
 const draggableItems = document.querySelectorAll('.item');
+
+function handleTouchStart(e) {
+  draggedItem = e.target;
+  draggedItem.style.left = ''; // スタイルをリセット
+  draggedItem.style.top = ''; // スタイルをリセット
+  let touch = e.touches[0];
+  startX = touch.clientX - parseFloat(draggedItem.style.left || 0);
+  startY = touch.clientY - parseFloat(draggedItem.style.top || 0);
+  e.preventDefault();
+}
+
+function handleTouchMove(e) {
+  if (!draggedItem) return;
+  let touch = e.touches[0];
+  draggedItem.style.left = `${touch.clientX - startX}px`;
+  draggedItem.style.top = `${touch.clientY - startY}px`;
+  e.preventDefault();
+  e.target.classList.add('active');
+}
+
 function handleTouchEnd(e) {
   e.target.classList.remove('active');
   if (!draggedItem) return;
